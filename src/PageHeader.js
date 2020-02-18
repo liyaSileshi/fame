2339fbf682e54c80209b30ef3ed16051f3eef83a
liyaSileshi <liyasileshi@gmail.com>
2020-02-18 00:07:05 -0800

    api stuff

---
+++ src/PageHeader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './PageHeader.css'
+
+function PageHeader() {
+    return (
+        <div className='PageHeader'>
+            <h1>
+                Fame Contest
+            </h1>
+        </div>
+    )
+}
+
+export default PageHeader
