18904244a9100856117713414c2552776e1ea024
liyaSileshi <liyasileshi@gmail.com>
2020-02-27 11:18:54 -0800

    Updates

---
+++ precache-manifest.19f83346320c8a3e61bf7b4a109954d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3725fd1e950072b5aef46b5502fd3423",
+    "url": "/fame/index.html"
+  },
+  {
+    "revision": "84a503c25bd4f2b0271e",
+    "url": "/fame/static/css/main.11c313f6.chunk.css"
+  },
+  {
+    "revision": "360483fb9fa0bd4ef410",
+    "url": "/fame/static/js/2.22e6f62b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/fame/static/js/2.22e6f62b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84a503c25bd4f2b0271e",
+    "url": "/fame/static/js/main.673966ed.chunk.js"
+  },
+  {
+    "revision": "e6566589b43421a587c0",
+    "url": "/fame/static/js/runtime-main.590539a2.js"
+  }
+]);
